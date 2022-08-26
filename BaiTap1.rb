e0e73c198355af1ae7040a2e9efd1c25cba725ac
hoanguyen <hoavannguyen682001@gmail.com>
2022-08-26 23:03:36 +0700

    first commit

---
+++ BaiTap1.rb
@@ -0,0 +1,21 @@
+puts "Xin chao - Nguyễn Văn Hoà - 1911505310217"
+
+print "Nhap vao so thu nhat: "
+num1 = gets.chomp()
+puts "So vua nhap:"+ num1
+
+print "Nhap vao so thu hai: "
+num2 = gets.chomp()
+puts "So vua nhap:"+ num2
+
+puts ("So sanh 2 so: " + num1 + " va " + num2 )
+
+if num1 > num2
+    puts (num1 +" lon hon " + num2)
+else
+    puts (num2 +" lon hon " + num1)
+end
+
+
+
+
