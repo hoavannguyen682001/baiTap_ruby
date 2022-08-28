fccc71054218b5dbec5604809249859bee088b80
hoanguyen <hoavannguyen682001@gmail.com>
2022-08-28 19:17:01 +0700

    fix lỗi nhập số

--- BaiTap1.rb
+++ BaiTap1.rb
@@ -1,5 +1,10 @@
-puts "Xin chao - Nguyễn Văn Hoà - 1911505310217"
 
+
+puts "1. Viết lệnh hiển thị Xin chào + tên SV + MSV"
+puts "Xin chao - Nguyễn Văn Hoà - 1911505310217"
+puts ""
+
+puts "2. Nhập 1 số từ bàn phím, in ra số đó"
 print "Nhap vao so thu nhat: "
 num1 = gets.chomp()
 puts "So vua nhap:"+ num1
@@ -7,13 +12,15 @@ puts "So vua nhap:"+ num1
 print "Nhap vao so thu hai: "
 num2 = gets.chomp()
 puts "So vua nhap:"+ num2
+puts ""
 
+puts "3. So sánh 2 giá trị nhập vào từ bàn phím, in ra giá trị lớn hơn"
 puts ("So sanh 2 so: " + num1 + " va " + num2 )
 
-if num1 > num2
-    puts (num1 +" lon hon " + num2)
+if num1.to_i > num2.to_i
+    puts ("So lon hon la: " + num1)
 else
-    puts (num2 +" lon hon " + num1)
+    puts ("So lon hon la: " + num2)
 end
 
 
